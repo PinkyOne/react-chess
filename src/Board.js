1befd4e6c47564f5348c68cf120bd6c56170e024
Alexander Elistratov <alexsanr56@gmail.com>
2017-04-25 19:59:46 +0400

    Implemented all with react

--- src/Board.js
+++ src/Board.js
@@ -8,29 +8,26 @@ import HTML5Backend from 'react-dnd-html5-backend';
 
 class Board extends Component {
   
- renderSquare(i) {
-  const x = i % 8;
-  const y = Math.floor(i / 8);
-  return (
-    <div key={i}
-         style={{ width: '12.5%', height: '12.5%' }}>
-      <BoardSquare x={x}
-                   y={y}>
-        {this.renderPiece(x, y)}
-      </BoardSquare>
-    </div>
-  );
-}
-
-renderPiece(x, y) {
-  const [knightX, knightY] = this.props.knightPosition;
-  if (x === knightX && y === knightY) {
-    return <Knight />;
+  renderSquare(i) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    return (
+      <div key={i}
+           style={{ width: '12.5%', height: '12.5%' }}>
+        <BoardSquare x={x}
+                     y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
+      </div>
+    );
   }
-}
 
-
-  
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y === knightY) {
+      return <Knight />;
+    }
+  }
 
   handleSquareClick(toX, toY) {
      if (canMoveKnight(toX, toY)) {
