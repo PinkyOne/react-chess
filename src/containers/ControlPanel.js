6a146bc5c35f60bf49eff4b65cd82a0ca810b3f1
Alexander Elistratov <alexsanr56@gmail.com>
2017-04-30 01:58:12 +0400

    Optimized control panel

--- src/containers/ControlPanel.js
+++ src/containers/ControlPanel.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import Button from "../components/Button";
-import PositionTextField from "../components/PositionTextField";
+import PositionTextField from "./PositionTextField";
 import moveKnight from '../actions'
 
 class ControlPanel extends Component {
@@ -15,22 +15,19 @@ class ControlPanel extends Component {
             (Math.abs(dx) === 1 && Math.abs(dy) === 2);
     }
 
-    setTextField(text) {
-        this.setState({positionText: text});
-    }
-
     setKnightPosition() {
         const positionText = this.props.knightPosition.text;
         if (!positionText)return;
         const letters = 'ABCDEFGH';
-        const y = letters.indexOf(positionText[0]);
-        const x = parseInt(positionText[1], 10) - 1;
-        if (!this.canMoveKnight(x, y)) {
-            alert('Knight move must be like a L');
+        const newY = letters.indexOf(positionText[0]);
+        const newX = parseInt(positionText[1], 10) - 1;
+        if (!this.canMoveKnight(newX, newY)) {
+            const {x, y} = this.props.knightPosition;
+            if (newX !== x || newY !== y)
+                alert('Knight move must be like a L');
             return false;
         }
-        this.props.moveKnight(x, y);
-        this.setTextField(positionText);
+        this.props.moveKnight(newX, newY);
     }
 
     render() {
